5e7d721a96d0815f43ae5be967a80c723379f820
brian <brian@debian-brian.brian>
2018-03-01 21:20:59 +0100

    Adding trips loading in map

--- HashCode2018/src/main.ts
+++ HashCode2018/src/main.ts
@@ -4,23 +4,4 @@ import { Car } from './Car';
 import { Map } from './Map';
 
 let map: Map = Map.init_map(process.argv[2]);
-var trip1: Trip = new Trip(new Vector2(0, 0), new Vector2(0, 3));
-var trip2: Trip = new Trip(new Vector2(5, 0), new Vector2(1, 3));
-var arr_trip: Trip[] = [trip1, trip2];
-var arr_veh: Car[] = [];
-
-arr_trip.forEach(element => {
-	console.log(element);
-});
-
-while (1) {
-	arr_veh.forEach(veh => {
-		var shortest_trips: Trip[] = check_shortest(arr_trip);
-		var free_trips: Trip = filter_freetrips(shortest_trips);
-		var trip: Trip = get_shortest(free_trip);
-		veh.trip = trip;
-	});
-	arr_veh.forEach(veh => {
-		veh.navigate();
-	});
-}
+console.log(map);
